f61f41be24561f486082103cb0a5428af0f4a030
Jaco Greeff <jacogr@gmail.com>
2019-02-20 14:55:01 +0100

    Dried Danta (#82)
    * Dried Danta
    
    * Remove (now-unused) chain config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+# 0.27.1
+
+Replace Charred Cherry with Dried Danta, remove old decimals and token name configs
+
+# 0.26.1
+
+Add keyring support for sd25519 (schnorrkel)
+
 # 0.25.1
 
 Add `@polkadot/{keyring,util,util-crypto,types}` as peerDependencies for the various packages as used. Thie should allow less churn on this repo with version bumps, although it does add some extra effort onto the users of these libraries. (But the assumption is that they use these anyway)
--- packages/ui-settings/src/defaults.ts
+++ packages/ui-settings/src/defaults.ts
@@ -2,38 +2,11 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { ChainsInfo, Options } from './types';
-
-const CHAINS: ChainsInfo = [
-  {
-    name: 'Development',
-    networkId: 42,
-    tokenDecimals: 0,
-    tokenSymbol: 'Unit'
-  },
-  {
-    name: 'Local Testnet',
-    networkId: 42,
-    tokenDecimals: 0,
-    tokenSymbol: 'Unit'
-  },
-  {
-    name: 'Charred Cherry',
-    networkId: 42,
-    tokenDecimals: 15,
-    tokenSymbol: 'CHR'
-  },
-  {
-    name: 'Alexander',
-    networkId: 42,
-    tokenDecimals: 15,
-    tokenSymbol: 'DOT'
-  }
-];
+import { Options } from './types';
 
 const ENDPOINTS: Options = [
   { text: 'Alexander (Polkadot, hosted by Parity)', value: 'wss://poc3-rpc.polkadot.io/' },
-  { text: 'Charred Cherry (Substrate, hosted by Parity)', value: 'wss://substrate-rpc.parity.io/' },
+  { text: 'Dried Danta (Substrate, hosted by Parity)', value: 'wss://substrate-rpc.parity.io/' },
   { text: 'Local Node (127.0.0.1:9944)', value: 'ws://127.0.0.1:9944/' }
 ];
 
@@ -52,7 +25,6 @@ const UITHEMES: Options = [
 ];
 
 export {
-  CHAINS,
   ENDPOINTS,
   LANGUAGES,
   UIMODES,
