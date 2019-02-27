f9c246c520f3114c2af921bc3bc7988261408c97
Jaco Greeff <jacogr@gmail.com>
2019-02-27 11:01:53 +0100

    Update KeyPair.tsx

--- packages/ui-keyring/src/options/KeyPair.tsx
+++ packages/ui-keyring/src/options/KeyPair.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import IdentityIcon from '@polkadot/ui-identicon/Identicon';
+import IdentityIcon from '@polkadot/ui-identicon/index';
 
 type Props = {
   address: string,
