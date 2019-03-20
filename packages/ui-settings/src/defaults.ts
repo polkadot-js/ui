b108f259bb01e2e37ddd0af85f88bff201765b54
Stefanie Doll <stefie.doll@googlemail.com>
2019-03-20 19:10:47 +0100

    brackets for readability

--- packages/ui-settings/src/defaults.ts
+++ packages/ui-settings/src/defaults.ts
@@ -5,7 +5,7 @@
 import { Options } from './types';
 
 // matches https://polkadot.js.org & https://poc-3.polkadot.io
-const isPolkadot = typeof window !== 'undefined' && window.location.host.indexOf('polkadot') !== -1;
+const isPolkadot = (typeof window !== 'undefined') && window.location.host.indexOf('polkadot') !== -1;
 
 const WSS_POLKADOT = 'wss://poc3-rpc.polkadot.io/';
 const WSS_SUBSTRATE = 'wss://substrate-rpc.parity.io/';
@@ -44,7 +44,7 @@ const UITHEME_DEFAULT = isPolkadot
   ? 'polkadot'
   : 'substrate';
 
-const UIMODE_DEFAULT = !isPolkadot && typeof window !== 'undefined' && window.location.host.indexOf('ui-light') !== -1
+const UIMODE_DEFAULT = !isPolkadot && (typeof window !== 'undefined') && window.location.host.indexOf('ui-light') !== -1
   ? 'light'
   : 'full';
 
