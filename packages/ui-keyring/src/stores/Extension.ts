f9c311eec9b90085245714c6d258a684ffb0852e
Jaco Greeff <jacogr@gmail.com>
2019-06-06 12:17:44 +0200

    Add ExtensionStore (from @polkadot/extension)

---
+++ packages/ui-keyring/src/stores/Extension.ts
@@ -0,0 +1,58 @@
+// Copyright 2019 @polkadot/ui-keyring authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { KeyringStore } from '../types';
+
+import extension from 'extensionizer';
+
+const lastError = (type: string): void => {
+  const error = extension.runtime.lastError;
+
+  if (error) {
+    console.error(`ExtensionStore.${type}:: runtime.lastError:`, error);
+  }
+};
+
+export default class ExtensionStore implements KeyringStore {
+  all (cb: (key: string, value: any) => void): void {
+    extension.storage.local.get(null, (result: { [index: string]: any }) => {
+      lastError('all');
+
+      Object.entries(result).forEach(([key, value]) =>
+        cb(key, value)
+      );
+    });
+  }
+
+  get (key: string, cb: (value: any) => void): void {
+    extension.storage.local.get([key], (result: { [index: string]: any }) => {
+      lastError('get');
+
+      cb(result[key]);
+    });
+  }
+
+  remove (key: string, cb?: () => void): void {
+    extension.storage.local.remove(key, () => {
+      lastError('remove');
+
+      cb && cb();
+    });
+  }
+
+  set (key: string, value: any, cb?: () => void): void {
+    // shortcut, don't save testing accounts in extension storage
+    if (key.indexOf('account:') === 0 && value.meta && value.meta.isTesting) {
+      cb && cb();
+
+      return;
+    }
+
+    extension.storage.local.set({ [key]: value }, () => {
+      lastError('set');
+
+      cb && cb();
+    });
+  }
+}
