f463038b58d154a52fab600e3d6a5472d73c07ba
Luke Sugiura <jun@dappface.com>
2019-11-22 14:48:59 +0100

    export SettingsStruct

--- packages/ui-settings/src/index.ts
+++ packages/ui-settings/src/index.ts
@@ -4,6 +4,7 @@
 
 import settings, { Settings } from './Settings';
 export { ENDPOINT_DEFAULT, ICON_DEFAULT, ICON_DEFAULT_HOST, LANGUAGE_DEFAULT, LOCKING_DEFAULT, PREFIX_DEFAULT, UIMODE_DEFAULT, UITHEME_DEFAULT } from './defaults';
+export { SettingsStruct } from './types'
 
 export default settings;
 
