c22f638b2f8616f86b7d4748d07b3cb650fa7390
puti <xiegq@s-labs.cn>
2020-01-15 20:27:36 +0800

    fix some bugs

--- packages/exampleReactNative/nodeGlobalsShim.js
+++ packages/exampleReactNative/nodeGlobalsShim.js
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 /* eslint-disable no-global-assign */
-
+require('fast-text-encoding');
 import AsyncStorage from '@react-native-community/async-storage';
 
 class Storage {
