fe4e6df90195fad5e0dd91fc879fb89775663ab0
Jaco Greeff <jacogr@gmail.com>
2020-01-28 18:08:00 +0100

    Bump deps (#265)

--- packages/ui-keyring/src/index.ts
+++ packages/ui-keyring/src/index.ts
@@ -2,12 +2,20 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { assertSingletonPackage } from '@polkadot/util';
+import { detectPackage } from '@polkadot/util';
 
 import keyring, { Keyring } from './Keyring';
 import Ledger from './ledger';
 
-assertSingletonPackage('@polkadot/ui-keyring');
+let pkgJson;
+
+try {
+  pkgJson = require('./package.json');
+} catch (error) {
+  pkgJson = require('../package.json');
+}
+
+detectPackage(pkgJson);
 
 export default keyring;
 
