de6d7a8e1ee90ccaf788b2827f8824438a7597e4
Hanwen Cheng <heawen.cheng@gmail.com>
2020-03-24 16:39:27 +0100

    make lint happy with spaces

--- packages/react-qr/src/NetworkSpecs.tsx
+++ packages/react-qr/src/NetworkSpecs.tsx
@@ -3,37 +3,37 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { NetworkSpecsStruct } from '@polkadot/ui-settings';
-import { BaseProps } from './types';
 
 import React, { useEffect, useState } from 'react';
-
-import {encodeString} from './util';
 import QrDisplay from './Display';
+import { BaseProps } from './types';
+
+import { encodeString } from './util';
 
 interface Props extends BaseProps {
-	networkSpecs: NetworkSpecsStruct
+  networkSpecs: NetworkSpecsStruct;
 }
 
-function DisplayNetworkSpecs ({ className, networkSpecs, size, style }: Props): React.ReactElement<Props> | null {
-	const [data, setData] = useState<Uint8Array | null>(null);
+function DisplayNetworkSpecs({ className, networkSpecs, size, style }: Props): React.ReactElement<Props> | null {
+  const [data, setData] = useState<Uint8Array | null>(null);
 
-	useEffect((): void => {
-		setData(encodeString(JSON.stringify(networkSpecs)));
-	}, [networkSpecs]);
+  useEffect((): void => {
+    setData(encodeString(JSON.stringify(networkSpecs)));
+  }, [networkSpecs]);
 
-	if (!data) {
-		return null;
-	}
+  if (!data) {
+    return null;
+  }
 
-	return (
-		<QrDisplay
-			className={className}
-			skipEncoding={true}
-			size={size}
-			style={style}
-			value={data}
-		/>
-	);
+  return (
+    <QrDisplay
+      className={className}
+      skipEncoding={true}
+      size={size}
+      style={style}
+      value={data}
+    />
+  );
 }
 
 export default React.memo(DisplayNetworkSpecs);
