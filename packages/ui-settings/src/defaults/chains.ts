e0f65d30c1dbdb5d5394ee4046264410094ef9ba
thib <thibaut.sardan@gmail.com>
2020-10-16 17:46:33 +0200

    much nicer chains

--- packages/ui-settings/src/defaults/chains.ts
+++ packages/ui-settings/src/defaults/chains.ts
@@ -1,15 +1,12 @@
 // Copyright 2017-2020 @polkadot/ui-settings authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { all } from '@polkadot/networks';
+import known from '@polkadot/networks';
 
 type ChainDef = string[];
 
-// Kusama is the only network to have several genesis hashes
-const kusamaHashes = all.filter(({ genesisHash }) => genesisHash && genesisHash.length > 1)?.[0].genesisHash;
-
-const chains: Record<string, ChainDef> = (!!kusamaHashes?.length && {
-  kusama: kusamaHashes
-}) || {};
+const chains: Record <string, ChainDef> = known
+  .filter(({ genesisHash }) => genesisHash.length > 1)
+  .reduce((chains, { genesisHash, network }) => ({ ...chains, [network]: genesisHash }), {});
 
 export default chains;
