4fe5924eb48e96f0a6771dbcf9ce5c8931ccb896
thib <thibaut.sardan@gmail.com>
2020-10-16 16:18:08 +0200

    adapt ss58

--- packages/ui-settings/src/defaults/ss58.ts
+++ packages/ui-settings/src/defaults/ss58.ts
@@ -3,45 +3,20 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { Option } from '../types';
+import { available } from '@polkadot/networks';
 
 export const PREFIX_DEFAULT = -1;
 
-export const PREFIXES: Option[] = [
-  {
-    info: 'default',
-    text: 'Default for the connected node',
-    value: -1
-  },
-  // keep as first (well, after default)
-  {
-    info: 'substrate',
-    text: 'Substrate (generic)',
-    value: 42
-  },
-  // all in ascending order based on value
-  {
-    info: 'polkadot',
-    text: 'Polkadot (live)',
-    value: 0
-  },
-  {
-    info: 'kusama',
-    text: 'Kusama (canary)',
-    value: 2
-  },
-  {
-    info: 'edgeware',
-    text: 'Edgeware (live)',
-    value: 7
-  },
-  {
-    info: 'kulupu',
-    text: 'Kulupu (live)',
-    value: 16
-  },
-  {
-    info: 'subsocial',
-    text: 'Subsocial (live)',
-    value: 28
-  }
-];
+const defaultNetwork: Option = {
+  info: 'default',
+  text: 'Default for the connected node',
+  value: -1
+};
+
+const networks = available.map(({ displayName, network, prefix }) => ({
+  info: network,
+  text: displayName,
+  value: prefix
+}));
+
+export const PREFIXES: Option[] = [defaultNetwork, ...networks];
