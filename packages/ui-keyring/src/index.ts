2c5b064cc96f399613f752c9801bc5008bffc93e
Jaco Greeff <jacogr@gmail.com>
2020-11-10 15:28:05 +0100

    Split Ledger (& transports) into package

--- packages/ui-keyring/src/index.ts
+++ packages/ui-keyring/src/index.ts
@@ -1,17 +1,12 @@
 // Copyright 2017-2020 @polkadot/ui-keyring authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { detectPackage } from '@polkadot/util';
+import './detectPackage';
 
 import keyring, { Keyring } from './Keyring';
-import Ledger from './ledger';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-detectPackage(require('./package.json'), typeof __dirname !== 'undefined' && __dirname);
+export { default as Ledger } from '@polkadot/ledger';
 
 export default keyring;
 
-export {
-  Keyring,
-  Ledger
-};
+export { Keyring };
