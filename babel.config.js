788b561b0e08f8bc2254d6b20a2b3710b815e084
Jaco Greeff <jacogr@gmail.com>
2020-11-11 11:33:00 +0100

    Update dev

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 // Copyright 2017-2020 @polkadot/ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-const base = require('@polkadot/dev/config/babel');
+const base = require('@polkadot/dev/config/babel-config-cjs.cjs');
 
 module.exports = Object.keys(base).reduce((config, key) => {
   config[key] = base[key];
