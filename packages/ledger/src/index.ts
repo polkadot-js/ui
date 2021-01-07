7842c3abf173674e3467f96fb397ade0eb33158e
lovesh <lovesh.bond@gmail.com>
2021-01-08 00:41:37 +0530

    Allow Ledger support for Dock
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- packages/ledger/src/index.ts
+++ packages/ledger/src/index.ts
@@ -4,7 +4,7 @@
 import type Transport from '@ledgerhq/hw-transport';
 import type { AccountOptions, LedgerAddress, LedgerSignature, LedgerTypes, LedgerVersion } from './types';
 
-import { newKusamaApp, newPolkadotApp, ResponseBase, SubstrateApp } from '@zondax/ledger-polkadot';
+import { newDockApp, newKusamaApp, newPolkadotApp, ResponseBase, SubstrateApp } from '@zondax/ledger-polkadot';
 
 import { transports } from '@polkadot/ledger-transports';
 import { assert, bufferToU8a, u8aToBuffer, u8aToHex } from '@polkadot/util';
@@ -18,6 +18,7 @@ export const LEDGER_DEFAULT_INDEX = 0x80000000;
 const SUCCESS_CODE = 0x9000;
 
 const APPS: Record<string, (transport: Transport) => SubstrateApp> = {
+  dock: newDockApp,
   kusama: newKusamaApp,
   polkadot: newPolkadotApp
 };
