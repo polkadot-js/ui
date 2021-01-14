19c68dada78522306e6c7d34d420294ad3bb3291
Jaco Greeff <jacogr@gmail.com>
2021-01-14 22:54:38 +0100

    Use @polkadot/hw-ledger from common (#429)

--- packages/ledger-transports/README.md
+++
@@ -1,2 +0,0 @@
-# @polkadot/ledger-transports
-
--- packages/ledger-transports/src/react-native.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2021 @polkadot/ledger authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import type { TransportDef } from './types';
-
-const transports: TransportDef[] = [];
-
-export { transports };
