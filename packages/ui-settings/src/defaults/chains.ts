d8237f0687c3105f429e2617eb64eef52ba9b382
Jaco Greeff <jacogr@gmail.com>
2021-02-07 14:55:32 +0100

    0.69 (#438)
    * 0.69
    
    * Adjust
    
    * Adjust
    
    * Update CHANGELOG.md

--- packages/ui-settings/src/defaults/chains.ts
+++ packages/ui-settings/src/defaults/chains.ts
@@ -1,12 +1,19 @@
 // Copyright 2017-2021 @polkadot/ui-settings authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { Network } from '@polkadot/networks/types';
+
 import known from '@polkadot/networks';
 
 type ChainDef = string[];
 
+// NOTE This should be fixed by networks 5.7 (also in ss58)
+interface NetworkNamed extends Network {
+  network: string;
+}
+
 const chains: Record <string, ChainDef> = known
-  .filter(({ genesisHash }) => genesisHash.length > 1)
+  .filter((n): n is NetworkNamed => n.genesisHash.length > 0 && !!n.network)
   .reduce((chains, { genesisHash, network }) => ({ ...chains, [network]: genesisHash }), {});
 
 export { chains };
--- packages/ui-settings/src/defaults/ss58.ts
+++ packages/ui-settings/src/defaults/ss58.ts
@@ -1,9 +1,15 @@
 // Copyright 2017-2021 @polkadot/ui-settings authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { Network } from '@polkadot/networks/types';
 import type { Option } from '../types';
 
 import { available } from '@polkadot/networks';
+
+// NOTE This should be fixed by networks 5.7 (also in chains)
+interface NetworkNamed extends Network {
+  network: string;
+}
 
 export const PREFIX_DEFAULT = -1;
 
@@ -13,10 +19,8 @@ const defaultNetwork: Option = {
   value: -1
 };
 
-const networks = available.map(({ displayName, network, prefix }) => ({
-  info: network,
-  text: displayName,
-  value: prefix
-}));
+const networks = available
+  .filter((n): n is NetworkNamed => !!n.network)
+  .map(({ displayName, network, prefix }) => ({ info: network, text: displayName, value: prefix }));
 
 export const PREFIXES: Option[] = [defaultNetwork, ...networks];
