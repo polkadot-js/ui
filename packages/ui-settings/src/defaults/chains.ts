52d37c8cd1b8c6dc11408c6a87c202b73c312a73
Jaco Greeff <jacogr@gmail.com>
2021-02-07 14:49:04 +0100

    Adjust

--- packages/ui-settings/src/defaults/chains.ts
+++ packages/ui-settings/src/defaults/chains.ts
@@ -1,18 +1,19 @@
 // Copyright 2017-2021 @polkadot/ui-settings authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { Network } from '@polkadot/networks/types';
+
 import known from '@polkadot/networks';
 
 type ChainDef = string[];
 
-interface NetDef {
-  genesisHash: string[];
+// NOTE This should be fixed by networks 5.7 (also in ss58)
+interface NetworkNamed extends Network {
   network: string;
 }
 
 const chains: Record <string, ChainDef> = known
-  .map(({ genesisHash, network }) => ({ genesisHash, network }))
-  .filter((def): def is NetDef => def.genesisHash.length > 0 && !!def.network)
+  .filter((n): n is NetworkNamed => n.genesisHash.length > 0 && !!n.network)
   .reduce((chains, { genesisHash, network }) => ({ ...chains, [network]: genesisHash }), {});
 
 export { chains };
