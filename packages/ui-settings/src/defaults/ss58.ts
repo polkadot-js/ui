a126155dbf305ab361ea0c028651e79332ac3690
Jaco Greeff <jacogr@gmail.com>
2021-02-08 17:45:55 +0100

    Bump util

--- packages/ui-settings/src/defaults/ss58.ts
+++ packages/ui-settings/src/defaults/ss58.ts
@@ -1,15 +1,9 @@
 // Copyright 2017-2021 @polkadot/ui-settings authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Network } from '@polkadot/networks/types';
 import type { Option } from '../types';
 
 import { available } from '@polkadot/networks';
-
-// NOTE This should be fixed by networks 5.7 (also in chains)
-interface NetworkNamed extends Network {
-  network: string;
-}
 
 export const PREFIX_DEFAULT = -1;
 
@@ -19,8 +13,10 @@ const defaultNetwork: Option = {
   value: -1
 };
 
-const networks = available
-  .filter((n): n is NetworkNamed => !!n.network)
-  .map(({ displayName, network, prefix }) => ({ info: network, text: displayName, value: prefix }));
+const networks = available.map(({ displayName, network, prefix }) => ({
+  info: network,
+  text: displayName,
+  value: prefix
+}));
 
 export const PREFIXES: Option[] = [defaultNetwork, ...networks];
