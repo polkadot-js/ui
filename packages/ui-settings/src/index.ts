0404d5dadd6900c62fdc2bd917e076e79901508e
wirednkod <wirednkod@gmail.com>
2021-06-01 15:20:42 +0300

    export types of possible endpoints

--- packages/ui-settings/src/index.ts
+++ packages/ui-settings/src/index.ts
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Settings, settings } from './Settings';
+import { Endpoint, JsonRpcEndpoint, SubstrateConnectEndpoint, EndpointType } from './types';
 
 export { ENDPOINT_DEFAULT, ICON_DEFAULT, ICON_DEFAULT_HOST, LANGUAGE_DEFAULT, LOCKING_DEFAULT, PREFIX_DEFAULT, UIMODE_DEFAULT, UITHEME_DEFAULT } from './defaults';
 export { packageInfo } from './packageInfo';
 
-export { settings, Settings };
+export { settings, Settings, Endpoint, JsonRpcEndpoint, SubstrateConnectEndpoint, EndpointType };
 
 export default settings;
