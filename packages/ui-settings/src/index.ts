c6497d65762272ab4e87a89c09b2b9f250e3a67a
wirednkod <wirednkod@gmail.com>
2021-06-03 09:31:07 +0300

    Fix lint error concerning imports

--- packages/ui-settings/src/index.ts
+++ packages/ui-settings/src/index.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Settings, settings } from './Settings';
-import { Endpoint, JsonRpcEndpoint, SubstrateConnectEndpoint, EndpointType } from './types';
+import { Endpoint, EndpointType, JsonRpcEndpoint, SubstrateConnectEndpoint } from './types';
 
 export { ENDPOINT_DEFAULT, ICON_DEFAULT, ICON_DEFAULT_HOST, LANGUAGE_DEFAULT, LOCKING_DEFAULT, PREFIX_DEFAULT, UIMODE_DEFAULT, UITHEME_DEFAULT } from './defaults';
 export { packageInfo } from './packageInfo';
