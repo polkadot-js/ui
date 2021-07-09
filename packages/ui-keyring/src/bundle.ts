be251919d682d5505c05a68a6c5541b3d7b775ad
Jaco <jacogr@gmail.com>
2021-07-09 15:44:16 +0300

    Add bundle.ts files for browser bundling (#502)

---
+++ packages/ui-keyring/src/bundle.ts
@@ -0,0 +1,11 @@
+// Copyright 2017-2021 @polkadot/ui-keyring authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { Keyring } from './Keyring';
+
+export { Ledger } from '@polkadot/hw-ledger';
+export { packageInfo } from './packageInfo';
+
+const keyring = new Keyring();
+
+export { Keyring, keyring };
