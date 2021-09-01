0e2543b6f1b4c3c529dff14ad0cf9e320af2f60f
Jaco Greeff <jacogr@gmail.com>
2021-09-01 19:27:13 +0300

    Jest config cleanup

--- jest.config.cjs
+++ jest.config.cjs
@@ -12,19 +12,5 @@ module.exports = {
     '\\.(css|less)$': 'empty/object',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'empty/object'
   },
-  modulePathIgnorePatterns: [
-    '<rootDir>/packages/example-react/build',
-    '<rootDir>/packages/example-vue/build',
-    '<rootDir>/packages/exampleReactNative/build',
-    '<rootDir>/packages/react-identicon/build',
-    '<rootDir>/packages/react-qr/build',
-    '<rootDir>/packages/reactnative-identicon/build',
-    '<rootDir>/packages/ui-assets/build',
-    '<rootDir>/packages/ui-keyring/build',
-    '<rootDir>/packages/ui-settings/build',
-    '<rootDir>/packages/ui-shared/build',
-    '<rootDir>/packages/vue-identicon/build'
-  ],
-  testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['/node_modules/(?!@polkadot|@babel/runtime/helpers/esm/)']
+  testEnvironment: 'jsdom'
 };
