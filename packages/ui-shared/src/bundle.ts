cbbe125da9fd5ddf10a72a590d68f7d9c2b6f2d5
Jaco <jacogr@gmail.com>
2022-01-02 08:33:26 +0100

    0.88.1 (#582)
    * 0.88.1
    
    * Apply bundle where missing

--- packages/ui-shared/src/bundle.ts
+++ packages/ui-shared/src/bundle.ts
@@ -1,6 +1,5 @@
 // Copyright 2018-2022 @polkadot/ui-shared authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import './detectPackage';
-
-export * from './bundle';
+export * from './icons';
+export { packageInfo } from './packageInfo';
