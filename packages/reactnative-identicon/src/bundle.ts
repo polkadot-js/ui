05971a23e2bf8a01966c2a1576fb1d6eb5236541
Jaco <jacogr@gmail.com>
2022-01-02 09:04:14 +0200

    Apply bundle where missing

--- packages/reactnative-identicon/src/bundle.ts
+++ packages/reactnative-identicon/src/bundle.ts
@@ -1,10 +1,5 @@
 // Copyright 2017-2022 @polkadot/reactnative-identicon authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import './detectPackage';
-
-import Identicon from './Identicon';
-
-export * from './bundle';
-
-export default Identicon;
+export * from './icons';
+export { packageInfo } from './packageInfo';
