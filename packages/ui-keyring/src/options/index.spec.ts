1c6fc693d4dd1b4cfd244a850eee7508173286cf
Jaco <jacogr@gmail.com>
2023-02-25 12:05:33 +0200

    Adjust tests for node:test env

--- packages/ui-keyring/src/options/index.spec.ts
+++ packages/ui-keyring/src/options/index.spec.ts
@@ -1,6 +1,9 @@
 // Copyright 2017-2023 @polkadot/ui-keyring authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// eslint-disable-next-line spaced-comment
+/// <reference types="@polkadot/dev/node/test/node" />
+
 import type { KeyringStruct } from '../types';
 
 import { KeyringOption } from '.';
