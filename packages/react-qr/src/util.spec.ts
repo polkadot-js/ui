8632ca7c7afa63c288533a78a6a63f22eb0df3a0
Jaco <jacogr@gmail.com>
2023-03-06 14:04:59 +0200

    Bump dev deps

--- packages/react-qr/src/util.spec.ts
+++ packages/react-qr/src/util.spec.ts
@@ -1,7 +1,6 @@
 // Copyright 2017-2023 @polkadot/react-qr authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-// eslint-disable-next-line spaced-comment
 /// <reference types="@polkadot/dev/node/test/node" />
 
 import { u8aConcat, u8aToHex, u8aToString } from '@polkadot/util';
