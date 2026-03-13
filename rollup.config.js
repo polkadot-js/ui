c5ab15118d3c9a99cf02aa543cdf61e0e168993a
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2026-03-13 09:56:35 -0400

    Set headers for 2026 (#859)
    * Set headers for 2026
    
    * remaining headers

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2025 @polkadot/ui-shared authors & contributors
+// Copyright 2017-2026 @polkadot/ui-shared authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import path from 'path';
